0f4b368d780283d5730e8a25fb8f05dc903c96ac
Christian Di Santo <disantochistian@gmail.com>
2021-11-18 23:33:06 +0100

    novedades

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenido <code>este sera muy pronto la pagina </code> de Christian Di Santo.
         </p>
         <a
           className="App-link"
